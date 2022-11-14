c277974b23d9400fc82a26a7101c0188a0b1702d
Pieter van Mill <pietervanmill@hotmail.com>
2022-11-14 19:05:33 +0100

    Add continent name to endpoint

--- src/routes/country/get.country.js
+++ src/routes/country/get.country.js
@@ -4,7 +4,6 @@ async function getCountry(req, res, next) {
     const client = await getClient();
     
     try {
-
         // check if the id exists
         const country = await client.query(`SELECT * FROM country WHERE id = $1`, [req.params.id]);
 
@@ -12,11 +11,17 @@ async function getCountry(req, res, next) {
             throw({ message: 'This country was not found!', status: 404 });
         }
 
+        const continent = await client.query(`
+            SELECT c.id, ct.name FROM continent AS c
+            INNER JOIN continent_translation AS ct ON ct.id = c.default_translation
+            WHERE c.id = $1`, [country.rows[0].continent_id]);
+
         const translations = await client.query(`SELECT id, name, created, updated, language_code FROM country_translation WHERE country_id = $1`, [req.params.id]);
 
         res.status(200);
         res.send({
             ...country.rows[0],
+            continent_name: continent.rows[0].name,
             translations: translations.rows
         });
     } catch (error) {
