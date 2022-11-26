d68662382d4dd1432cec41fd64f1d1b6c9e56b6c
Pieter van Mill <pietervanmill@hotmail.com>
2022-11-26 10:13:41 +0100

    Flip numbers so it works with the north east south west format.

--- src/routes/place/get.bbox.js
+++ src/routes/place/get.bbox.js
@@ -9,18 +9,18 @@ async function getBoundingBox(req, res, next) {
             throw({ message: 'Missing stuff', status: 400 });
         }
 
-        const corner1 = req.body.boundingbox[0];
-        const corner2 = req.body.boundingbox[1];
+        const northWest = req.body.boundingbox[0];
+        const southEast = req.body.boundingbox[1];
 
-        if (corner1.length !== 2 || corner2.length !== 2) {
+        if (northWest.length !== 2 || southEast.length !== 2) {
             throw({ message: 'Invalid bounding box', status: 400 });
         }
 
         try {
-            Number(corner1[0]);
-            Number(corner1[1]);
-            Number(corner2[0]);
-            Number(corner2[1]);
+            Number(northWest[0]);
+            Number(northWest[1]);
+            Number(southEast[0]);
+            Number(southEast[1]);
         } catch (error) {
             throw({ message: 'Invalid coordinates', status: 400});
         }
@@ -29,11 +29,11 @@ async function getBoundingBox(req, res, next) {
         SELECT p.id, pt.name, p.latitude, p.longitude, p.population
         FROM place AS p
         INNER JOIN place_translation AS pt ON pt.id = p.default_translation
-        WHERE p.latitude > $1
-        AND p.latitude < $2
-        AND p.longitude > $3
-        AND p.longitude < $4
-        `, [corner1[0], corner2[0], corner1[1], corner2[1]]);
+        WHERE p.latitude < $1
+        AND p.latitude > $2
+        AND p.longitude < $3
+        AND p.longitude > $4
+        `, [northWest[0], southEast[0], northWest[1], southEast[1]]);
         
         res.status(200);
         res.send({
