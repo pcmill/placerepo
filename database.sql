2b035e0545a10c04e8663cea875b89d72981bbe7
Pieter van Mill <pietervanmill@hotmail.com>
2022-11-28 19:30:00 +0100

    Add SQL

--- database.sql
+++ database.sql
@@ -33,7 +33,7 @@ CREATE TABLE "country_translation" (
   "country_id" varchar(4) NOT NULL
 );
 
-CREATE TABLE "admin1" (
+CREATE TABLE "admin" (
   "id" varchar(6) UNIQUE PRIMARY KEY,
   "default_translation" varchar(12),
   "created" timestamp NOT NULL,
@@ -41,79 +41,17 @@ CREATE TABLE "admin1" (
   "latitude" decimal(8,6) NOT NULL,
   "longitude" decimal(9,6) NOT NULL,
   "polygon" text,
-  "country_id" varchar(4) NOT NULL
-);
-
-CREATE TABLE "admin1_translation" (
-  "id" varchar(12) UNIQUE PRIMARY KEY,
-  "name" varchar(128) NOT NULL,
-  "created" timestamp NOT NULL,
-  "updated" timestamp,
-  "language_code" varchar(5) NOT NULL,
-  "admin1_id" varchar(6) NOT NULL
-);
-
-CREATE TABLE "admin2" (
-  "id" varchar(6) UNIQUE PRIMARY KEY,
-  "default_translation" varchar(12),
-  "created" timestamp NOT NULL,
-  "updated" timestamp,
-  "polygon" text,
-  "latitude" decimal(8,6) NOT NULL,
-  "longitude" decimal(9,6) NOT NULL,
   "country_id" varchar(4) NOT NULL,
-  "falls_under_admin" varchar(6) NOT NULL
+  "admin_id" varchar(6)
 );
 
-CREATE TABLE "admin2_translation" (
+CREATE TABLE "admin_translation" (
   "id" varchar(12) UNIQUE PRIMARY KEY,
   "name" varchar(128) NOT NULL,
   "created" timestamp NOT NULL,
   "updated" timestamp,
   "language_code" varchar(5) NOT NULL,
-  "admin2_id" varchar(6) NOT NULL
-);
-
-CREATE TABLE "admin3" (
-  "id" varchar(6) UNIQUE PRIMARY KEY,
-  "default_translation" varchar(12),
-  "created" timestamp NOT NULL,
-  "updated" timestamp,
-  "polygon" text,
-  "latitude" decimal(8,6) NOT NULL,
-  "longitude" decimal(9,6) NOT NULL,
-  "country_id" varchar(4) NOT NULL,
-  "falls_under_admin" varchar(6) NOT NULL
-);
-
-CREATE TABLE "admin3_translation" (
-  "id" varchar(12) UNIQUE PRIMARY KEY,
-  "name" varchar(128) NOT NULL,
-  "created" timestamp NOT NULL,
-  "updated" timestamp,
-  "language_code" varchar(5) NOT NULL,
-  "admin3_id" varchar(6) NOT NULL
-);
-
-CREATE TABLE "admin4" (
-  "id" varchar(6) UNIQUE PRIMARY KEY,
-  "default_translation" varchar(12),
-  "created" timestamp NOT NULL,
-  "updated" timestamp,
-  "polygon" text,
-  "latitude" decimal(8,6) NOT NULL,
-  "longitude" decimal(9,6) NOT NULL,
-  "country_id" varchar(4) NOT NULL,
-  "falls_under_admin" varchar(6) NOT NULL
-);
-
-CREATE TABLE "admin4_translation" (
-  "id" varchar(12) UNIQUE PRIMARY KEY,
-  "name" varchar(128) NOT NULL,
-  "created" timestamp NOT NULL,
-  "updated" timestamp,
-  "language_code" varchar(5) NOT NULL,
-  "admin4_id" varchar(6) NOT NULL
+  "admin_id" varchar(6) NOT NULL
 );
 
 CREATE TABLE "place" (
@@ -129,10 +67,7 @@ CREATE TABLE "place" (
   "polygon" text,
   "wikidata_id" varchar(20),
   "timezone" varchar(40),
-  "admin1" varchar(6) NOT NULL,
-  "admin2" varchar(6) NOT NULL,
-  "admin3" varchar(6),
-  "admin4" varchar(6),
+  "admin" varchar(6) NOT NULL,
   "country_id" varchar(4) NOT NULL
 );
 
@@ -177,23 +112,17 @@ CREATE TABLE "postal_code" (
   "country_id" varchar(4) NOT NULL
 );
 
+CREATE TABLE "language" (
+  "id" serial UNIQUE PRIMARY KEY,
+  "language_code" varchar(5),
+  "description" varchar(256)
+);
+
 ALTER TABLE "country" ADD FOREIGN KEY ("continent_id") REFERENCES "continent" ("id");
 
 ALTER TABLE "place" ADD FOREIGN KEY ("country_id") REFERENCES "country" ("id");
 
 ALTER TABLE "postal_code" ADD FOREIGN KEY ("country_id") REFERENCES "country" ("id");
-
-ALTER TABLE "admin1" ADD FOREIGN KEY ("country_id") REFERENCES "country" ("id");
-
-ALTER TABLE "admin2" ADD FOREIGN KEY ("country_id") REFERENCES "country" ("id");
-
-ALTER TABLE "admin3" ADD FOREIGN KEY ("country_id") REFERENCES "country" ("id");
-
-ALTER TABLE "admin4" ADD FOREIGN KEY ("country_id") REFERENCES "country" ("id");
-
-ALTER TABLE "admin1_translation" ADD FOREIGN KEY ("admin1_id") REFERENCES "admin1" ("id");
-
-ALTER TABLE "admin2_translation" ADD FOREIGN KEY ("admin2_id") REFERENCES "admin2" ("id");
 
 ALTER TABLE "neighbourhood" ADD FOREIGN KEY ("place_id") REFERENCES "place" ("id");
 
@@ -209,24 +138,16 @@ ALTER TABLE "continent" ADD FOREIGN KEY ("default_translation") REFERENCES "cont
 
 ALTER TABLE "country" ADD FOREIGN KEY ("default_translation") REFERENCES "country_translation" ("id");
 
-ALTER TABLE "admin1" ADD FOREIGN KEY ("default_translation") REFERENCES "admin1_translation" ("id");
-
-ALTER TABLE "admin2" ADD FOREIGN KEY ("default_translation") REFERENCES "admin2_translation" ("id");
-
-ALTER TABLE "admin3_translation" ADD FOREIGN KEY ("admin3_id") REFERENCES "admin3" ("id");
-
-ALTER TABLE "admin3" ADD FOREIGN KEY ("default_translation") REFERENCES "admin3_translation" ("id");
-
-ALTER TABLE "admin4" ADD FOREIGN KEY ("default_translation") REFERENCES "admin4_translation" ("id");
-
-ALTER TABLE "admin4_translation" ADD FOREIGN KEY ("admin4_id") REFERENCES "admin4" ("id");
-
 ALTER TABLE "place" ADD FOREIGN KEY ("default_translation") REFERENCES "place_translation" ("id");
 
 ALTER TABLE "neighbourhood" ADD FOREIGN KEY ("default_translation") REFERENCES "neighbourhood_translation" ("id");
 
-ALTER TABLE "admin4" ADD FOREIGN KEY ("admin3_id") REFERENCES "admin3" ("id");
+ALTER TABLE "admin_translation" ADD FOREIGN KEY ("admin_id") REFERENCES "admin" ("id");
 
-ALTER TABLE "admin3" ADD FOREIGN KEY ("admin2_id") REFERENCES "admin2" ("id");
+ALTER TABLE "admin" ADD FOREIGN KEY ("country_id") REFERENCES "country" ("id");
 
-ALTER TABLE "admin2" ADD FOREIGN KEY ("admin1_id") REFERENCES "admin1" ("id");
+ALTER TABLE "admin" ADD FOREIGN KEY ("default_translation") REFERENCES "admin_translation" ("id");
+
+ALTER TABLE "place" ADD FOREIGN KEY ("admin") REFERENCES "admin" ("id");
+
+ALTER TABLE "admin" ADD FOREIGN KEY ("admin_id") REFERENCES "admin" ("id");
