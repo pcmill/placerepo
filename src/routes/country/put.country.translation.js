c85a975de737bd255ce10c347362feafb58e1d00
Pieter van Mill <pietervanmill@hotmail.com>
2022-12-05 20:37:15 +0100

    Edit translation endpoints updated.

--- src/routes/country/put.country.translation.js
+++ src/routes/country/put.country.translation.js
@@ -4,7 +4,7 @@ async function updateCountryTranslation(req, res, next) {
     const client = await getClient();
 
     try {
-        if (!req.body || !req.body.name || !req.body.language_code || !req.body.translation_id) {
+        if (!req.body || !req.body.name || !req.body.translation_id) {
             throw({ message: 'Missing stuff', status: 400 });
         }
 
@@ -17,22 +17,14 @@ async function updateCountryTranslation(req, res, next) {
             throw({ message: 'This country translation was not found!', status: 404 });
         }
 
-        if (req.body.language_code.length < 2) {
-            throw({ message: 'Language code should be at least 2 characters.', status: 400 });
-        }
-
-        if (req.body.language_code.length > 5) {
-            throw({ message: 'Language code can be no longer than 5 characters.', status: 400 });
-        }
-
         if (req.body.name.length > 128) {
             throw({ message: 'Name can be no longer than 128 characters.', status: 400 });
         }
 
         await client.query(`
             UPDATE country_translation
-            SET name = $1, language_code = $2, updated = NOW()
-            WHERE id = $3`, [req.body.name, req.body.language_code, req.body.translation_id]);
+            SET name = $1, updated = NOW()
+            WHERE id = $2`, [req.body.name, req.body.translation_id]);
 
         await client.query('COMMIT');
 
