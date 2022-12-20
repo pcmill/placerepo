b23385b35058a1ce99a2ff47bf24f51034c278c9
Pieter van Mill <pietervanmill@hotmail.com>
2022-12-20 12:32:09 +0100

    Redirect to the right place.

--- src/routes/auth/github.auth.js
+++ src/routes/auth/github.auth.js
@@ -26,7 +26,7 @@ async function getGithubAuth(req, res, next) {
         await client.query('COMMIT');
 
         // redirect to the frontend
-        res.redirect(`http://localhost:3000/auth/github?access_token=${response.access_token}&user_id=${user.id}`);
+        res.redirect(`${FRONTEND_URL}/auth/github?access_token=${response.access_token}&user_id=${user.id}`);
     } catch (error) {
         next(error);
     } finally {
