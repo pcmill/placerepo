0fe7ef7e27d0248544ff5de76030c83228329266
Pieter van Mill <pietervanmill@hotmail.com>
2022-12-25 12:03:46 +0100

    Also send ID for queue item.

--- src/routes/queue/get.queue.js
+++ src/routes/queue/get.queue.js
@@ -6,16 +6,16 @@ async function getQueue(req, res, next) {
     try {
         // check if the id exists
         const queue = await client.query(`
-            SELECT q.request, q.created, q.request_type, c.github_user_name, c.github_avatar
+            SELECT q.id, q.request, q.created, q.request_type, c.github_user_name, c.github_avatar
             FROM queue AS q
             INNER JOIN contributor AS c ON c.github_user_id = q.user_id
             WHERE status = $1
         `, [0]);
 
         res.status(200);
-        res.send({
-            ...queue.rows[0]
-        });
+        res.send([
+            ...queue.rows
+        ]);
     } catch (error) {
         next(error);
     } finally {
