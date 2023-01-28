67a918e49ef7d834b9862e0f27a254f8ca8b6979
Pieter van Mill <pietervanmill@hotmail.com>
2023-01-28 13:57:49 +0100

    Switch to different elevation API.

--- src/util/elevation.js
+++ src/util/elevation.js
@@ -3,11 +3,11 @@ import fetch from "node-fetch";
 export async function getElevation(latitude, longitude) {
     return new Promise(async (resolve, reject) => {
         try {
-            const result = await fetch(`https://api.open-elevation.com/api/v1/lookup?locations=${latitude},${longitude}`);
+            const result = await fetch(`https://api.opentopodata.org/v1/etopo1?locations=${latitude},${longitude}`);
             const data = await result.json();
 
             if (result.ok && data && data.results && data.results[0].elevation) {
-                resolve(data.results[0].elevation);
+                resolve(Number(data.results[0].elevation));
             } else {
                 console.log('No elevation found for', latitude, longitude);
                 resolve(0);
