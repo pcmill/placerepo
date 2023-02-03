07ec5685a42705ac7d4cc011fa82297e6fbc19b4
Pieter van Mill <pietervanmill@hotmail.com>
2023-02-03 19:01:52 +0100

    Actually every 30 min.

--- src/index.js
+++ src/index.js
@@ -55,7 +55,7 @@ if (process.env.NODE_ENV === 'production') {
 
     console.log('Starting cronjob Meilisearch update');
     
-    cron.schedule('30 * * * *', async () => {
+    cron.schedule('*/30 * * * *', async () => {
         await updateMeilisearch();
     });
 }
